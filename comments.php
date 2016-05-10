dc92728ce053ef959b6489e65147ca07e08c83f9
Nancy Ouyang <nancy.ouyang@gmail.com>
2016-05-09 23:37:06 -0400

    removing unnecessary stuff for single user

--- comments.php
+++ comments.php
@@ -2,6 +2,19 @@
 	if (post_password_required())
 		return;
 ?>
+<style>
+#author, #email, #url{/* css 3 */
+ border-radius:5px;
+ /* mozilla */
+ -moz-border-radius:5px;
+ /* webkit */
+ -webkit-border-radius:5px;
+ box-shadow: none;
+border: none;
+background: #fafafa;
+ }
+
+</style>
 
 <div id="comments">
 	<?php if (have_comments()) : ?>
@@ -15,34 +28,30 @@
 			<li class="previous"><?php previous_comments_link(__( '&laquo; Older Comments', 'ipin')); ?></li>
 			<li class="next"><?php next_comments_link(__('Newer Comments &raquo;', 'ipin')); ?></li>
 		</ul>
-		<?php endif;?>
+		<?php endif;?><!-- #comment-nav-above -->
 
 	<?php
 	elseif (!comments_open() && '0' != get_comments_number() && post_type_supports(get_post_type(), 'comments')) :
 	endif;
 	
-	if (is_user_logged_in()) {
+	if (comments_open()) { //if open for comments, display form
 		global $user_ID;
 
 		comment_form(array(
 		'title_reply' => '',
 		'title_reply_to' => '',
 		'cancel_reply_link' => __('X Cancel reply', 'ipin'),
-		'comment_notes_before' => '',
-		'comment_notes_after' => '',
-		'logged_in_as' => '',
+		'comment_notes_before' => '<hr/><p class="comment-notes">' . __( 'Your email address will not be published.' ) . ( $req ? $required_text : '' ) . '</p>',
+		//'comment_notes_after' => '',
+		//'logged_in_as' => '',
 		'label_submit' => __('Post Comment', 'ipin'),
-		'comment_field' => '<div class="pull-left">' . get_avatar($user_ID, '48') . '</div>' . '<div class="textarea-wrapper"><textarea class="form-control" placeholder="' . __('Add a comment...', 'ipin') . '" id="comment" name="comment" aria-required="true"></textarea></div>'
-		));
-	} else if (comments_open()) {
+		'comment_field' => '<div class="pull-left">' . get_avatar($user_ID, '48') . '</div>' . '<div class="textarea-wrapper"><textarea class="form-control" placeholder="' . __('Add a comment...', 'ipin') . '" id="comment" name="comment" aria-required="true"></textarea></div><br/>'
+		)
+
+		);
+	} else {
 	?>
-		<form method="post" id="commentform">
-			<div class="pull-left"><?php echo get_avatar('', '48'); ?></div>
-			<div class="textarea-wrapper">
-				<textarea class="form-control" disabled placeholder="<?php _e('Login to comment...', 'ipin'); ?>"></textarea>
-				<button id="submit" class="btn btn-success" type="submit"><?php _e('Post Comment', 'ipin'); ?></button>
-			</div>
-		</form>
+			<p class="no-comments"><?php _e( 'Comments are closed.', 'ipin' ); ?></p>
 	<?php
 	}
 	?>
